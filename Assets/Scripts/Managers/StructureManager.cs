8249fdcf2a93dc8e93b3e3e279ffdd6226adeaed
topr.nontawat <topr.nontawat@gmail.com>
2023-07-01 14:58:21 +0700

    Grid Structure

--- Assets/Scripts/Managers/StructureManager.cs
+++ Assets/Scripts/Managers/StructureManager.cs
@@ -4,15 +4,98 @@ using UnityEngine;
 
 public class StructureManager : MonoBehaviour
 {
+    [SerializeField] private bool isConstructing;
+    [SerializeField] private bool isDemolishing;
+
+    [SerializeField] private GameObject curBuildingPrefab;
+    [SerializeField] private GameObject buildingParent;
+
+    [SerializeField] private Vector3 curCursorPos;
+
+    public GameObject buildingCursor;
+    public GameObject gridPlane;
+
+    private GameObject ghostBuilding;
+
+    [SerializeField] private GameObject _curStructure;
+
+    public GameObject CurStructure{
+        get {return _curStructure;}
+        set {_curStructure = value;}
+    }
+
+    [SerializeField] private GameObject[] structurePrefab;
+    private Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Escape))
+            CancelStructureMode();
+
+        curCursorPos = Formula.instance.GetCurTilePosition();
+        if(isConstructing){
+            buildingCursor.transform.position = curCursorPos;
+            gridPlane.SetActive(true);
+        }else{
+            gridPlane.SetActive(false);
+        }
+
+        CheckLeftClick();
+        CheckRightClick();
+    }
+
+    public void BeginNewBuildingPlacement(GameObject prefab){
+        isDemolishing = false;
+        isConstructing = true;
+
+        curBuildingPrefab = prefab;
+
+        ghostBuilding = Instantiate(curBuildingPrefab, curCursorPos, Quaternion.identity);
+        ghostBuilding.GetComponent<FindBuildingSite>().Plane.SetActive(true);
+
+        buildingCursor = ghostBuilding;
+        buildingCursor.SetActive(true);
+    }
+
+    public void PlaceBuilding(){
+        if(!buildingCursor.GetComponent<FindBuildingSite>().CanBuild)
+            return;
         
+        GameObject structureObj = Instantiate(curBuildingPrefab, curCursorPos, ghostBuilding.transform.rotation, buildingParent.transform);
+
+        Structure s = structureObj.GetComponent<Structure>();
+    }
+
+    private void CheckLeftClick(){
+        if(Input.GetMouseButtonDown(0)){
+            if(isConstructing)
+                PlaceBuilding();
+        }
+    }
+
+    private void CancelStructureMode(){
+        isConstructing = false;
+        if(buildingCursor != null)
+            buildingCursor.SetActive(false);
+
+        if(ghostBuilding != null)
+            Destroy(ghostBuilding);
+    }
+
+    private void RotateBuilding(){
+        ghostBuilding.transform.rotation *= Quaternion.Euler(Vector3.up * 90);
+    }
+
+    private void CheckRightClick(){
+        if(Input.GetMouseButtonDown(1)){
+            RotateBuilding();
+        }
     }
 }
